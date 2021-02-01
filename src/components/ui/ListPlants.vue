94d781ca3ff0d361ddae3e7c3e09db4b43e61f65
Manuel Rúa <manuruaco@gmail.com>
2021-02-01 19:25:21 +0100

    Setup & First Component

---
+++ src/components/ui/ListPlants.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="title font-bold text-xl mb-4">
+      {{ title }}
+    </h1>
+    <ul>
+      <li>
+        <button class="border border-black rounded-2xl py-2 px-4 text-3xl">
+          +
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class ListPlants extends Vue {
+  title = 'Mis plantas'
+}
+</script>
